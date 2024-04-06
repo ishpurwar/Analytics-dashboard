a0fbcf418995d5a170db97f388610f3a08632a24
Ish Purwar <96423154+ishpurwar@users.noreply.github.com>
2024-04-06 14:53:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   // IMPORTANT: This analytics dashboard is compatible with any app, your main app lives here!
 
-  return <main className="text-white">This is an example</main>
+  return <main className="text-white">This is an visit page to go analytics dashboard add /analytics on the url</main>
 }
